cf5f78fe2747ec14cac628ba55973382267e06fd
Evertton Matheus <evertton.g.l@hotmail.com>
2025-11-01 15:32:00 -0400

    Update scrapping.js
    add args: ['--no-sandbox', '--disable-setuid-sandbox']

--- scrapping.js
+++ scrapping.js
@@ -27,6 +27,7 @@ export async function runScrapping() {
   const browser = await puppeteer.launch({
     headless: true,
     defaultViewport: { width: 1366, height: 800 },
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
   });
 
   const page = await browser.newPage();
